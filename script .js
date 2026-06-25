4e2a2dd1ef1b65da0ebcb3b06cd89b83fb0aadaf
augustinejohn851-lgtm <augustinejohn851@gmail.com>
2026-06-25 12:58:26 +0100

    Timeclock

--- script .js
+++ script .js
@@ -1,32 +1,120 @@
-let music = document.getElementById("player");
+// Digital Clock
+
+function updateClock(){
+
+let now = new Date();
 
 
-function play(){
-    music.play();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+
+document.getElementById("time").innerHTML =
+hours + ":" +
+minutes.toString().padStart(2,'0') + ":" +
+seconds.toString().padStart(2,'0');
+
+
+document.getElementById("date").innerHTML =
+now.toDateString();
+
+
 }
 
 
-function pause(){
-    music.pause();
+setInterval(updateClock,1000);
+
+
+
+// Attendance System
+
+function checkIn(){
+
+let time = new Date().toLocaleTimeString();
+
+document.getElementById("status").innerHTML =
+"Checked In at " + time;
+
 }
 
 
-function changeSong(file,name){
 
-    music.src = file;
+function checkOut(){
 
-    document.getElementById("songName").innerHTML = name;
+let time = new Date().toLocaleTimeString();
 
-    music.play();
+document.getElementById("status").innerHTML =
+"Checked Out at " + time;
 
 }
 
 
-function next(){
-    alert("Next song");
+
+// Stopwatch
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timer;
+
+
+function startTimer(){
+
+timer = setInterval(function(){
+
+seconds++;
+
+
+if(seconds == 60){
+
+seconds = 0;
+minutes++;
+
 }
 
 
-function previous(){
-    alert("Previous song");
+if(minutes == 60){
+
+minutes = 0;
+hours++;
+
+}
+
+
+document.getElementById("timer").innerHTML =
+hours.toString().padStart(2,'0') + ":" +
+minutes.toString().padStart(2,'0') + ":" +
+seconds.toString().padStart(2,'0');
+
+
+},1000);
+
+
+}
+
+
+
+function stopTimer(){
+
+clearInterval(timer);
+
+}
+
+
+
+function resetTimer(){
+
+clearInterval(timer);
+
+seconds = 0;
+minutes = 0;
+hours = 0;
+
+
+document.getElementById("timer").innerHTML =
+"00:00:00";
+
+
 }
